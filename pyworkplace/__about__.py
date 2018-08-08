e73f0c462aa7196b6ffcfba42b3b3c80c7784002
@slovacus <slovacus@gmail.com>
2018-08-08 12:42:24 -0500

    (#3) Add: code for pyworkplace

---
+++ pyworkplace/__about__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+__package_name__ = 'pyworkplace'
+__version__ = '0.0.0'
+__author__ = '@slovacus'
+__author_email__ = 'slovacus@gmail.com'
+__description__ = ("""Pyworkplace is a wrapper for facebook""")
+__url__ = 'https://github.com/hadenlabs/pyworkplace'
---
+++ pyworkplace/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Pyworkplace - For workplace.
+
+"""
+__version__ = '0.4.0'
+__author__ = 'Luis Mayta'
+__email__ = 'slovacus@gmail.com'
+__licence__ = 'Mit'
+
+from pyworkplace.pyworkplace import Page

edb74bbfbeda66e781b426757970d7956933c0a4
@slovacus <slovacus@gmail.com>
2018-08-08 12:44:32 -0500

    (#3) Add: tests for pyworkplace

---
+++ tests/message/test_template.py
@@ -0,0 +1,273 @@
+# -*- coding: utf-8 -*-
+from hamcrest import assert_that, has_entries, not_none
+
+from pyworkplace.message import Template
+
+RECIPIENT_ID = 10000000
+URL = ''
+
+
+def test_send_generic_template():
+
+    params = {
+        'version': 'v2.8',
+        'access_token': 'this is my token',
+    }
+    message = Template(**params)
+
+    args = {
+        'recipient_id': RECIPIENT_ID,
+        'elements': [
+            {
+                'title': 'Title',
+                'image_url': 'https://hadenlabs.com/image1.png',
+                'subtitle': 'subtitle.',
+                'default_action': {
+                    'type': 'web_url',
+                    'url': 'https://hadenlabs.com/item/103',
+                    'messenger_extensions': True,
+                    'webview_height_ratio': 'tall',
+                    'fallback_url': 'https://hadenlabs.com/',
+                },
+                'buttons': [
+                    {
+                        'type': 'web_url',
+                        'url': 'https://url.com',
+                        'title': 'title',
+                    }, {
+                        'type': 'postback',
+                        'title': 'title',
+                        'payload': 'payload',
+                    },
+                ],
+            },
+        ],
+    }
+    assert_that(
+        message.send_generic_message(**args),
+        not_none(),
+    )
+    body = {
+        'message': {
+            'attachment': {
+                'type': 'template',
+                'payload': {
+                    'template_type': 'generic',
+                    'elements': [
+                        {
+                            'title': 'Title',
+                            'image_url': 'https://hadenlabs.com/image1.png',
+                            'subtitle': 'subtitle.',
+                            'default_action': {
+                                'type': 'web_url',
+                                'url': 'https://hadenlabs.com/item/103',
+                                'messenger_extensions': True,
+                                'webview_height_ratio': 'tall',
+                                'fallback_url': 'https://hadenlabs.com/',
+                            },
+                            'buttons': [
+                                {
+                                    'type': 'web_url',
+                                    'url': 'https://url.com',
+                                    'title': 'title',
+                                }, {
+                                    'type': 'postback',
+                                    'title': 'title',
+                                    'payload': 'payload',
+                                },
+                            ],
+                        },
+                    ],
+                },
+            },
+        },
+        'recipient': {'id': RECIPIENT_ID},
+        'notification_type': 'REGULAR',
+
+    }
+
+    response = {
+        'url': 'https://graph.facebook.com/v2.8/me/messages',
+        'params': {'access_token': params['access_token']},
+        'json': body,
+        'headers': {'Content-type': 'application/json'},
+        'method': 'post',
+    }
+    assert_that(
+        response,
+        has_entries(message.response),
+    )
+
+
+def test_send_generic_template_multiple():
+
+    params = {
+        'version': 'v2.8',
+        'access_token': 'this is my token',
+    }
+    message = Template(**params)
+
+    args = {
+        'recipient_id': RECIPIENT_ID,
+        'elements': [
+            {
+                'title': 'Title',
+                'image_url': 'https://hadenlabs.com/image1.png',
+                'subtitle': 'subtitle.',
+                'default_action': {
+                    'type': 'web_url',
+                    'url': 'https://hadenlabs.com/item/103',
+                    'messenger_extensions': True,
+                    'webview_height_ratio': 'tall',
+                    'fallback_url': 'https://hadenlabs.com/',
+                },
+                'buttons': [
+                    {
+                        'type': 'web_url',
+                        'url': 'https://url.com',
+                        'title': 'title',
+                    }, {
+                        'type': 'postback',
+                        'title': 'title',
+                        'payload': 'payload',
+                    },
+                ],
+            },
+            {
+                'title': 'Title2',
+                'image_url': 'https://hadenlabs.com/image2.png',
+                'subtitle': 'subtitle.',
+                'buttons': [
+                    {
+                        'type': 'postback',
+                        'title': 'title',
+                        'payload': 'payload',
+                    },
+                ],
+            },
+        ],
+    }
+    assert_that(
+        message.send_generic_message(**args),
+        not_none(),
+    )
+    body = {
+        'message': {
+            'attachment': {
+                'type': 'template',
+                'payload': {
+                    'template_type': 'generic',
+                    'elements': [
+                        {
+                            'title': 'Title',
+                            'image_url': 'https://hadenlabs.com/image1.png',
+                            'subtitle': 'subtitle.',
+                            'default_action': {
+                                'type': 'web_url',
+                                'url': 'https://hadenlabs.com/item/103',
+                                'messenger_extensions': True,
+                                'webview_height_ratio': 'tall',
+                                'fallback_url': 'https://hadenlabs.com/',
+                            },
+                            'buttons': [
+                                {
+                                    'type': 'web_url',
+                                    'url': 'https://url.com',
+                                    'title': 'title',
+                                }, {
+                                    'type': 'postback',
+                                    'title': 'title',
+                                    'payload': 'payload',
+                                },
+                            ],
+                        },
+                        {
+                            'title': 'Title2',
+                            'image_url': 'https://hadenlabs.com/image2.png',
+                            'subtitle': 'subtitle.',
+                            'buttons': [
+                                {
+                                    'type': 'postback',
+                                    'title': 'title',
+                                    'payload': 'payload',
+                                },
+                            ],
+                        },
+                    ],
+                },
+            },
+        },
+        'recipient': {'id': RECIPIENT_ID},
+        'notification_type': 'REGULAR',
+
+    }
+
+    response = {
+        'url': 'https://graph.facebook.com/v2.8/me/messages',
+        'params': {'access_token': params['access_token']},
+        'json': body,
+        'headers': {'Content-type': 'application/json'},
+        'method': 'post',
+    }
+    assert_that(
+        response,
+        has_entries(message.response),
+    )
+
+
+def test_send_button():
+
+    params = {
+        'version': 'v2.8',
+        'access_token': 'this is my token',
+    }
+    message = Template(**params)
+
+    args = {
+        'recipient_id': RECIPIENT_ID,
+        'message': 'button',
+        'buttons': [
+            {
+                'type': 'postback',
+                'title': 'title',
+                'payload': 'POSTBACK',
+            },
+        ],
+    }
+    assert_that(
+        message.send_button(**args),
+        not_none(),
+    )
+    body = {
+        'message': {
+            'attachment': {
+                'type': 'template',
+                'payload': {
+                    'template_type': 'button',
+                    'text': 'button',
+                    'buttons': [
+                        {
+                            'type': 'postback',
+                            'title': 'title',
+                            'payload': 'POSTBACK',
+                        },
+                    ],
+                },
+            },
+        },
+        'recipient': {'id': RECIPIENT_ID},
+        'notification_type': 'REGULAR',
+
+    }
+
+    response = {
+        'url': 'https://graph.facebook.com/v2.8/me/messages',
+        'params': {'access_token': params['access_token']},
+        'json': body,
+        'headers': {'Content-type': 'application/json'},
+        'method': 'post',
+    }
+    assert_that(
+        response,
+        has_entries(message.response),
+    )
